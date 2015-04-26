2fdfe92e1368a3e607b1f8ecd24d1b9550dbdc14
Auliman Arga <fukunokaze@hotmail.com>
2015-04-26 16:57:30 +0000

    git-tfs-id: [https://aulimanarga.visualstudio.com/]$/Exercise/SportsStore;C12

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -66,5 +66,10 @@ namespace SportsStore.WebUI.Controllers
             return cart;
         }
 
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
+
     }
 }
